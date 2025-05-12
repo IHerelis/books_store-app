8c94a184ed249c674c58456ddcfbac5c999ec924
IHerelis <agiresubito@gmail.com>
2025-05-12 21:09:16 +0300

    happy start project

---
+++ src/components/header/HomeHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomeHeader.css';
+
+const HomeHeader = () => {
+  return (
+    <div className='home-header__container'>
+      <h1>Happy start project !</h1>
+    </div>
+  );
+}
+
+export default HomeHeader;
---
+++ src/components/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './HomePage.css';
+
+const HomePage = () => {
+
+
+  return (
+    <div className='main-container'>
+      <div className='home-page__container'>
+        <h1>This is Main Page</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
